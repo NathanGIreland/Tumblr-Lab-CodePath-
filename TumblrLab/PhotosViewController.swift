ece8955ef0b14d1d90571a0603087728fe34749a
Nathan Ireland <nathangireland@gmail.com>
2019-10-14 15:50:24 -0500

    fixes

--- TumblrLab/PhotosViewController.swift
+++ TumblrLab/PhotosViewController.swift
@@ -18,7 +18,7 @@ class PhotosViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    // MARK: - Private Functions
+    // MARK: - Private Functionsh
     
     private func retrievePosts(){
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
